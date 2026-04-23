9005c5883f60e2e6e691b89794efa991766fbe5d
PerroBetto <milo8600@gmail.com>
2026-04-23 12:05:49 -0600

    Initialize enemy creation branch and created files for two simple enemies

---
+++ Dungeon-Crawler/src/entities/coral.py
@@ -0,0 +1,9 @@
+"""FIXME"""
+from pathlib import Path
+from typing import Any
+from math import sin
+
+import pygame
+from pygame import Vector2, Surface, Rect
+
+from entities.entity_mod import Entity
---
+++ Dungeon-Crawler/src/entities/urchin.py
@@ -0,0 +1,13 @@
+"""FIXME"""
+from pathlib import Path
+from typing import Any
+from math import sin
+
+import pygame
+from pygame import Vector2, Surface, Rect
+
+from entities.entity_mod import Entity
+
+
+class Urchin(Entity):
+    """Urchin enemy"""
